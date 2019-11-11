0f7d0b8ee84737016c78e81e381a5a4f714bddef
sandyadzim <sandy.adzim@gmail.com>
2019-11-11 20:46:10 +0700

    update

--- migrations/20191111122549-create-episode.js
+++ migrations/20191111122549-create-episode.js
@@ -36,10 +36,12 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
+        defaultValue: new Date(),
         type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
+        defaultValue: new Date(),
         type: Sequelize.DATE
       }
     });
